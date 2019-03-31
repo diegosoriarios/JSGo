7077a53018c50f1bf617d77961634bb060aed565
Diego Soria Rios <diegosoriarios@gmail.com>
2019-03-30 21:43:18 -0300

    Adding limit room

--- server.js
+++ server.js
@@ -34,61 +34,63 @@ let random = Math.floor(Math.random() * 11);
 let power = powerUp[random]
 
 io.on('connection', socket => {
-	clients.push(socket)
-	console.log('Socket Connectado' + socket.id)
+	if(clients.length < 3){
+		clients.push(socket)
+		console.log('Socket Connectado' + socket.id)
 
-	console.log('Clients', clients.length)
+		console.log('Clients', clients.length)
 
-	socket.on('sendPosition', data => {
-		//console.log(data)
-		socket.broadcast.emit('position', data);
-		let sort = Math.floor(Math.random() * 100);
-		sort = 7;
-		if(sort === 7){
-			if(!hasPowerUp){
-				io.emit('powerUpSend', power)
-				hasPowerUp = true;
+		socket.on('sendPosition', data => {
+			//console.log(data)
+			socket.broadcast.emit('position', data);
+			let sort = Math.floor(Math.random() * 100);
+			sort = 7;
+			if(sort === 7){
+				if(!hasPowerUp){
+					io.emit('powerUpSend', power)
+					hasPowerUp = true;
+				}
 			}
-		}
-	})
+		})
 
-	socket.on('blood', data => {
-		let x = data.x;
-		let y = data.y;
-		let blood = []
-		for(var i = 0; i < 32; i++){
-			var numX = Math.floor(Math.random()* 15) + 1;
-			var numY = Math.floor(Math.random()* 15) + 1;
-			numX *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
-			numY *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
-			blood.push({x: x + (i * 4), y: y + (i * 4), w: 4, h: 4, speedX: numX, speedY: numY})
-			x = x + (i * 4) - Math.floor(Math.random() * 32) + 4
-			x *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
-			y = y + (i * 4) - Math.floor(Math.random() * 32) + 4
-			y *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
-			blood.push({x: x, y: y, w: 4, h: 4, speedX: 0, speedY: 0})
-		}
-		io.emit('addBlood', blood)
-	})
+		socket.on('blood', data => {
+			let x = data.x;
+			let y = data.y;
+			let blood = []
+			for(var i = 0; i < 32; i++){
+				var numX = Math.floor(Math.random()* 15) + 1;
+				var numY = Math.floor(Math.random()* 15) + 1;
+				numX *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
+				numY *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
+				blood.push({x: x + (i * 4), y: y + (i * 4), w: 4, h: 4, speedX: numX, speedY: numY})
+				x = x + (i * 4) - Math.floor(Math.random() * 32) + 4
+				x *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
+				y = y + (i * 4) - Math.floor(Math.random() * 32) + 4
+				y *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
+				blood.push({x: x, y: y, w: 4, h: 4, speedX: 0, speedY: 0})
+			}
+			io.emit('addBlood', blood)
+		})
 
-	socket.on('getPower', data => {
-		random = Math.floor(Math.random() * 11);
-		power = powerUp[random]
-		hasPowerUp = false;
+		socket.on('getPower', data => {
+			random = Math.floor(Math.random() * 11);
+			power = powerUp[random]
+			hasPowerUp = false;
 
-	})
+		})
 
-	socket.on('shot', data => {
-		console.log(data)
-		data.hp -= 10;
-		socket.broadcast.emit('position', data);
-	})
+		socket.on('shot', data => {
+			console.log(data)
+			data.hp -= 10;
+			socket.broadcast.emit('position', data);
+		})
 
-	socket.on('disconnect', () => {
-		console.log(`${socket.id} disconnecting`)
-		var i = clients.indexOf(socket);
-		clients.splice(i, 1);
-	})
+		socket.on('disconnect', () => {
+			console.log(`${socket.id} disconnecting`)
+			var i = clients.indexOf(socket);
+			clients.splice(i, 1);
+		})
+	}	
 })
 
 server.listen(3000, () => {
